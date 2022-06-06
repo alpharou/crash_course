1e293416e4ef86dd6358b03784b04883874c6c6c
alpharou <alpharou.dev@gmail.com>
2022-06-07 01:26:18 +0200

    Minor warning fix

--- immutable_by_default.rs
+++ immutable_by_default.rs
@@ -8,10 +8,12 @@ pub fn run() {
 
 	let mut y: i32 = 34;
 
+	println!("First value of mutable variable y: {}", y);
+
 	y = 89; //This line creates a warning,
 	//Rust compiler understands that previous value
 	//was not used before reassigning the variable
 
-	println!("mutable variable y: {}", y);
+	println!("Second value of mutable variable y: {}", y);
 
 }
